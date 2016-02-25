10cfbde3c1f13bbd7fb3b45d9506ec2baf2e7518
greg.goray@gmail.com <greg.goray@gmail.com>
2016-02-25 15:08:05 +0100

    gregory

---
+++ src/com/KinoXP/view/AddMoveFormView.java
@@ -0,0 +1,127 @@
+package com.KinoXP.view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * Created by quena on 25-02-2016.
+ */
+public class AddMoveFormView {
+
+
+        private TextField moviePosterField;
+        private Scene scene;
+        Connection connection= null;
+
+        public void connectDB() {
+            String DB_URL = "jdbc:mysql://localhost/northmedia";
+            String USER = "root";
+            String PASS = "";
+            try {
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+        public Scene getNewMovieView() {
+            Label label1, label2;
+            label1 = new Label("NEW MOVIE");
+            label2 = new Label("");
+            label1.setFont(Font.font("Courier", FontWeight.BOLD, 25));
+            label1.setAlignment(Pos.CENTER);
+            label2.setFont(Font.font(14));
+            TextField text1, text2, text3, text4, text6, text7, text8;
+            TextArea textArea5;
+            HBox hBox1, hBox2, hBox3, hBox4;
+            hBox1 = new HBox();
+            hBox2 = new HBox();
+            hBox3 = new HBox();
+            hBox4 = new HBox();
+
+
+
+            VBox layout1, vbox1;
+            layout1 = new VBox();
+            vbox1 = new VBox(10);
+            BorderPane mainLayout = new BorderPane();
+            mainLayout.setPadding(new Insets(25));
+            mainLayout.setCenter(layout1);
+            Button buttonAdd, buttonBack, buttonAddActor;
+
+
+            scene = new Scene(mainLayout, 600, 450);
+
+
+            text1 = new TextField();
+            text1.setPromptText("Title");
+            text2 = new TextField();
+            text2.setPromptText("Year");
+            text3 = new TextField();
+            text3.setPromptText("Duration");
+            text4 = new TextField();
+            text4.setPromptText("Genre");
+            textArea5 = new TextArea();
+            textArea5.setPromptText("Description");
+            textArea5.setMaxSize(600, 200);
+            text6 = new TextField();
+            text6.setPromptText("Director");
+            text7 = new TextField();
+            text7.setPromptText("Age limit");
+            text8 = new TextField();
+            text8.setPromptText("Cast");
+
+            //Gregory
+            moviePosterField = new TextField();
+            moviePosterField.setPromptText("Paste posters URL");
+            Button posterURLButton = new Button("Add");
+            posterURLButton.setOnAction(event1 -> addPosterUrl(moviePosterField.getText()));
+            HBox posterHBox = new HBox();
+            posterHBox.getChildren().addAll(moviePosterField, posterURLButton);
+            //
+
+            buttonAdd = new Button("Add Movie");
+            buttonBack = new Button("Back");
+            buttonAddActor = new Button("Add");
+            buttonAddActor.setOnAction(event -> label2.setText("Actor : " + text8.getText() + " added!"));
+
+
+
+            layout1.getChildren().addAll(label1, text1, hBox1, textArea5, hBox2, hBox4, posterHBox , hBox3);
+            layout1.setSpacing(20);
+            hBox1.getChildren().addAll(text2, text3, text4);
+            hBox1.setSpacing(30);
+            hBox2.getChildren().addAll(text6, text7);
+            hBox2.setSpacing(30);
+            hBox3.getChildren().addAll(buttonAdd, buttonBack);
+            hBox3.setAlignment(Pos.BOTTOM_RIGHT);
+            hBox4.getChildren().addAll(text8, buttonAddActor, label2);
+
+            return scene;
+        }
+
+        public void addPosterUrl(String posterUrl) {
+            System.out.println(posterUrl);
+        }
+
+}
+
+
--- src/com/KinoXP/view/NewMovieView.java
+++ src/com/KinoXP/view/NewMovieView.java
@@ -1,7 +1,137 @@
 package com.KinoXP.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
 /**
- * Created by hartyandi on 2/24/16.
+ * Created by quena on 25-02-2016.
  */
 public class NewMovieView {
+    /**
+     *      ManageMovieView
+     *      by Grzegorz Goraj
+     */
+
+
+    Scene scene;
+    BorderPane borderPane;
+    HBox topLayoutHBox;
+    FlowPane flowPane;
+    Button plusButton;
+    Button logOut;
+    Label topLayout;
+    Stage primaryStage;
+    //CONTROLLERS
+
+    //
+
+
+
+    public void start(Stage primaryStage) throws Exception {
+        //ROOT
+        borderPane = new BorderPane();
+
+        //TOP
+        topLayout = new Label("T o p   L a y o u t");
+        topLayout.setPrefSize(1180, 30);
+        topLayout.setAlignment(Pos.CENTER);
+        topLayout.setStyle("-fx-background-color: azure");
+        logOut = new Button("Log Out");
+        logOut.setAlignment(Pos.CENTER_RIGHT);
+        topLayoutHBox = new HBox();
+        topLayoutHBox.setSpacing(10);
+        topLayoutHBox.getChildren().addAll(topLayout, logOut);
+        borderPane.setTop(topLayoutHBox);
+
+        //BORDER Left
+        plusButton = new Button("+");
+        plusButton.setPrefSize(40, 40);
+        plusButton.setOnAction(event -> addMovie());
+        borderPane.setAlignment(plusButton, Pos.CENTER);
+        borderPane.setLeft(plusButton);
+
+
+
+        //Center - FlowPane
+        flowPane = new FlowPane();
+        flowPane.setHgap(30);
+        flowPane.setVgap(30);
+        flowPane.setPadding(new Insets(60));
+        flowPane.setMaxWidth(1500);
+        borderPane.setCenter(flowPane);
+
+
+        //SCENE
+        scene = new Scene(borderPane, 1280, 800 );
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+    public void addMovie() {
+        //primaryStage.setScene(newMovieViewController.getCtrlNewMovieView());
+
+        VBox vBox = new VBox();
+        vBox.setPrefSize(80, 120);
+        vBox.setAlignment(Pos.CENTER);
+
+        Label titleBtnLabel = new Label();
+        titleBtnLabel.setPrefSize(80, 20);
+        titleBtnLabel.setTextAlignment(TextAlignment.RIGHT);
+        titleBtnLabel.setText("Ip-Man 3");
+
+        //Image imageOk = new Image(getClass().getResourceAsStream("moviePoster.png"));
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new URL("https://thehande.files.wordpress.com/2014/01/brucelee.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error loading image");
+        }
+
+        WritableImage wr = null;
+        if (img != null) {
+            wr = new WritableImage(img.getWidth(), img.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < img.getWidth(); x++) {
+                for (int y = 0; y < img.getHeight(); y++) {
+                    pw.setArgb(x, y, img.getRGB(x, y));
+                }
+            }
+        }
+
+        ImageView imageView = new ImageView();
+        imageView.setImage(wr);
+        imageView.setFitHeight(100);
+        imageView.setPreserveRatio(true);
+        imageView.setCache(true);
+
+        Button button = new Button();
+        button.setGraphic(imageView);
+        button.setPrefSize(80,100);
+        button.setPadding(new Insets(0,0,0,0));
+
+        vBox.getChildren().addAll(button, titleBtnLabel);
+        flowPane.getChildren().add(vBox);
+    }
 }
+
+
