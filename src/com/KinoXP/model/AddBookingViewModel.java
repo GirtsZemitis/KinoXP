20278cbb4eb4cbb6609753411f0d24d7a0b593c1
ghartyandi <g.hartyandi@gmail.com>
2016-03-01 12:22:20 +0100

    searching in booking expect date

--- src/com/KinoXP/model/AddBookingViewModel.java
+++ src/com/KinoXP/model/AddBookingViewModel.java
@@ -36,12 +36,16 @@ public class AddBookingViewModel {
     public Booking getBooking(String phoneNumber){
         String out = "";
         try {
-            String query = "SELECT * FROM Booking WHERE phoneNumber=?";
+            String query = "SELECT * FROM Booking WHERE phone_number=?";
             PreparedStatement preparedStatement = conn.prepareStatement(query);
             preparedStatement.setString(1, phoneNumber);
             ResultSet results = preparedStatement.executeQuery();
 
-            Booking booking = new Booking (results.getString(1),results.getString(2),results.getString(3),results.getInt(4),results.getString(5));
+            while(results.next()){
+                booking = new Booking (results.getString(1),results.getString(2),results.getString(3),results.getInt(4),results.getString(5));
+            }
+
+
 
             return booking;
 
