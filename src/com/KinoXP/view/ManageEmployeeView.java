2ae0898e12eaf984a91ba9ea90f2fc4d0acc4706
Ppa13 <patrunjel.paula@gmail.com>
2016-03-03 16:20:59 +0100

    manageEmployee classes finished

--- src/com/KinoXP/view/ManageEmployeeView.java
+++ src/com/KinoXP/view/ManageEmployeeView.java
@@ -20,7 +20,6 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
-
 /**
  * Created by Palko on 01/03/2016.
  */
@@ -30,33 +29,33 @@ public class ManageEmployeeView {
     private ManageEmployeeModel manageEmployeeModel;
 
 
-    private TableView<EmployeeModel>tableView = new TableView<>();
-    private TableView<LoginViewModel>tableView1 = new TableView<>();
+    private TableView<EmployeeModel> tableView = new TableView<>();
+    private TableView<LoginViewModel> tableView1 = new TableView<>();
 
 
-
-
-    public ManageEmployeeView(){
+    public ManageEmployeeView() {
         manageEmployeeModel = new ManageEmployeeModel();
         manageEmployeeController = new ManageEmployeeController(manageEmployeeModel, this);
     }
 
     public void start() {
 
-        //CREATING BORDER PANE WITH HBOX-CENTERED/BUTTOM AND GRIDPANE CENTERED IN THE MIDDLE
+        //CREATING BORDER PANE WITH H-BOX CENTERED/BUTTON AND GRID PANE CENTERED IN THE MIDDLE
         Stage primaryStage = new Stage();
         HBox hBox1 = new HBox();
-        hBox1.setPrefSize(600,80);
+        hBox1.setPrefSize(600, 80);
         HBox hBox = new HBox(20);
-        hBox.setPrefSize(600,80);
+        hBox.setPrefSize(600, 80);
         BorderPane borderPane = new BorderPane();
 
 
         Button logOut = new Button("LOG OUT");
         logOut.setStyle("-fx-font-size: 10");
+        Button goBack = new Button("GO BACK");
+        goBack.setStyle("-fx-font-size: 10");
         Button add = new Button("ADD");
         add.setStyle("-fx-font-size: 18");
-        Button delete =new Button("DELETE");
+        Button delete = new Button("DELETE");
         delete.setStyle("-fx-font-size:  18");
         Button edit = new Button("EDIT");
         edit.setStyle("-fx-font-size: 18");
@@ -67,10 +66,10 @@ public class ManageEmployeeView {
         Tab tab1 = new Tab();
         tab1.setText("LOGIN INFO");
         tab.setText("EMPLOYEE INFO");
-        tab.setContent(new Rectangle(200,200, Color.LIGHTSTEELBLUE));
-        tab.setContent(new Cylinder(200,200));
+        tab.setContent(new Rectangle(200, 200, Color.LIGHTSTEELBLUE));
+        tab.setContent(new Cylinder(200, 200));
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-        tabPane.getTabs().addAll(tab,tab1);
+        tabPane.getTabs().addAll(tab, tab1);
 
         tabPane.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
 
@@ -91,119 +90,113 @@ public class ManageEmployeeView {
         });
 
         //EDIT BOTH EMPLOYEE INFORMATION AND LOG IN INFORMATION
-
-        edit.setOnAction(event ->{
-
-            Stage stage1 = new Stage();
-            GridPane gridPane = new GridPane();
-            Label name = new Label("Name");
-            TextField nameTextField = new TextField();
-            Label surname = new Label("Surname");
-            TextField surnameText = new TextField();
-            Label email = new Label("Email");
-            TextField emailText = new TextField();
-            Label phoneNumber = new Label("Phone number");
-            TextField phoneNumberText = new TextField();
-            Label position = new Label("Position");
-            TextField positionTextField = new TextField();
-            Button confirm = new Button("Confirm");
-            Label userName = new Label("User name");
-            TextField userNameTextField = new TextField();
-            Label password = new Label("Password");
-            TextField passwordTextField = new TextField();
-
-            GridPane.setConstraints(name,0,0);
-            GridPane.setConstraints(nameTextField,1,0);
-            GridPane.setConstraints(surname,0,1);
-            GridPane.setConstraints(surnameText,1,1);
-            GridPane.setConstraints(email,0,2);
-            GridPane.setConstraints(emailText,1,2);
-            GridPane.setConstraints(phoneNumber,0,3);
-            GridPane.setConstraints(phoneNumberText,1,3);
-            GridPane.setConstraints(position,0,4);
-            GridPane.setConstraints(positionTextField,1,4);
-            GridPane.setConstraints(userName,0,5);
-            GridPane.setConstraints(userNameTextField,1,5);
-            GridPane.setConstraints(password,0,6);
-            GridPane.setConstraints(passwordTextField,1,6);
-            GridPane.setConstraints(confirm,1,7);
-
+        edit.setOnAction(event -> {
             EmployeeModel employeeModel = tableView.getSelectionModel().getSelectedItem();
+            if (employeeModel != null) {
+
+                Stage stage1 = new Stage();
+                GridPane gridPane = new GridPane();
+                Label name = new Label("Name");
+                TextField nameTextField = new TextField();
+                Label surname = new Label("Surname");
+                TextField surnameText = new TextField();
+                Label email = new Label("Email");
+                TextField emailText = new TextField();
+                Label phoneNumber = new Label("Phone number");
+                TextField phoneNumberText = new TextField();
+                Label position = new Label("Position");
+                TextField positionTextField = new TextField();
+                Button confirm = new Button("Confirm");
+                Label userName = new Label("User name");
+                TextField userNameTextField = new TextField();
+                Label password = new Label("Password");
+                TextField passwordTextField = new TextField();
+
+                GridPane.setConstraints(name, 0, 0);
+                GridPane.setConstraints(nameTextField, 1, 0);
+                GridPane.setConstraints(surname, 0, 1);
+                GridPane.setConstraints(surnameText, 1, 1);
+                GridPane.setConstraints(email, 0, 2);
+                GridPane.setConstraints(emailText, 1, 2);
+                GridPane.setConstraints(phoneNumber, 0, 3);
+                GridPane.setConstraints(phoneNumberText, 1, 3);
+                GridPane.setConstraints(position, 0, 4);
+                GridPane.setConstraints(positionTextField, 1, 4);
+                GridPane.setConstraints(userName, 0, 5);
+                GridPane.setConstraints(userNameTextField, 1, 5);
+                GridPane.setConstraints(password, 0, 6);
+                GridPane.setConstraints(passwordTextField, 1, 6);
+                GridPane.setConstraints(confirm, 1, 7);
+
+                userNameTextField.setText(employeeModel.getUserName());
+                nameTextField.setText(employeeModel.getName());
+                surnameText.setText(employeeModel.getSurname());
+                emailText.setText(employeeModel.getEmail());
+                phoneNumberText.setText(String.valueOf(employeeModel.getPhoneNumber()));
+                positionTextField.setText(employeeModel.getJobTitle());
+                passwordTextField.setText(manageEmployeeController.getPassword(employeeModel.getUserName()));
 
 
-
-            userNameTextField.setText(employeeModel.getUserName());
-            nameTextField.setText(employeeModel.getName());
-            surnameText.setText(employeeModel.getSurname());
-            emailText.setText(employeeModel.getEmail());
-            phoneNumberText.setText(String.valueOf(employeeModel.getPhoneNumber()));
-            positionTextField.setText(employeeModel.getJobTitle());
-            passwordTextField.setText(manageEmployeeController.getPassword(employeeModel.getUserName()));
+                confirm.setStyle("-fx-font-size: 20");
 
 
-            confirm.setStyle("-fx-font-size: 20");
+                gridPane.setAlignment(Pos.CENTER);
 
+                gridPane.getChildren().addAll(name, nameTextField, surname, surnameText, email, emailText, phoneNumber, phoneNumberText, position, userName, userNameTextField, password, passwordTextField, positionTextField, confirm);
 
+                gridPane.setHgap(20);
+                gridPane.setVgap(30);
+                Scene scene = new Scene(gridPane, 1000, 650);
 
-            gridPane.setAlignment(Pos.CENTER);
+                stage1.setScene(scene);
+                stage1.show();
 
-            gridPane.getChildren().addAll(name,nameTextField,surname,surnameText,email,emailText,phoneNumber,phoneNumberText,position,userName,userNameTextField,password,passwordTextField,positionTextField,confirm);
+                confirm.setOnAction(event1 -> {
+                    manageEmployeeController.editEmployee(userNameTextField.getText(), nameTextField.getText(), surnameText.getText(), emailText.getText(), Integer.parseInt(phoneNumberText.getText()), positionTextField.getText(), employeeModel.getUserName());
+                    manageEmployeeController.editLogin(userNameTextField.getText(), passwordTextField.getText(), employeeModel.getUserName());
 
-            gridPane.setHgap(20);
-            gridPane.setVgap(30);
-            Scene scene = new Scene(gridPane,1000,650);
+                    tableView.setItems(manageEmployeeController.returnEmployeeInfo());
+                    tableView1.setItems(manageEmployeeController.returnLogInInfo());
+                    stage1.close();
 
-            stage1.setScene(scene);
-            stage1.show();
+                });
 
-                    confirm.setOnAction(event1 -> {
+            } else {
+                updateAlertMessage("You need to select a row in order to edit an employee");
+            }
+        });
 
-                        manageEmployeeController.editEmployee(userNameTextField.getText(),nameTextField.getText(),surnameText.getText(),emailText.getText(),Integer.parseInt(phoneNumberText.getText()),positionTextField.getText(),employeeModel.getUserName());
-                        manageEmployeeController.editLogin(userNameTextField.getText(),passwordTextField.getText(),employeeModel.getUserName());
+        //DELETE EMPLOYEE AND LOG IN INFORMATION
+        delete.setOnAction(event -> {
+            EmployeeModel employeeModel = tableView.getSelectionModel().getSelectedItem();
+            if (employeeModel != null) {
 
+                //DELETE ROW FROM TABLE VIEW
+                manageEmployeeController.returnEmployeeInfo().remove(tableView.getSelectionModel().getSelectedItem());
 
+                //DELETE SELECTED INFORMATION FROM DATABASE
+                manageEmployeeController.deleteEmployeeAction(tableView.getSelectionModel().getSelectedItem().getUserName());
+                manageEmployeeController.deleteLogInCredentials(tableView.getSelectionModel().getSelectedItem().getUserName());
 
-                        tableView.setItems(manageEmployeeController.returnEmployeeInfo());
-                        tableView1.setItems(manageEmployeeController.returnLogInInfo());
-                stage1.close();
+                //ADD THE NEW INFORMATION TO THE TABLE VIEW
+                tableView.setItems(manageEmployeeController.returnEmployeeInfo());
+                tableView1.setItems(manageEmployeeModel.getLoginInformation());
 
-            });
+            } else {
+                updateAlertMessage("You need to select a row in order to delete an employee");
+            }
 
+        });
 
-
-
-    });
-
-        //deletes chosen rows
-
-        delete.setOnAction(event-> {
-
-
-            manageEmployeeController.returnEmployeeInfo().remove(tableView.getSelectionModel().getSelectedItem());
-
-            ManageEmployeeModel manageEmployeeModel = new ManageEmployeeModel();
-
-            manageEmployeeModel.deleteEmployee(tableView.getSelectionModel().getSelectedItem().getUserName());
-            String user = tableView.getSelectionModel().getSelectedItem().getUserName();
-            tableView.setItems(manageEmployeeController.returnEmployeeInfo());
-
-            //DELETE FROM DATABASE AND UPDATE LOGIN INFO TABLE VIEW
-            manageEmployeeModel.deleteLogInCredentials(user);
-            tableView1.setItems(manageEmployeeModel.getLoginInformation());
-
-
-    });
-
-        hBox.getChildren().addAll(add,edit,delete,logOut);
+        hBox.getChildren().addAll(goBack, add, edit, delete, logOut);
 
         borderPane.setBottom(hBox);
         tab.setContent(showBookings());
         tab1.setContent(showLogInInfo());
 
-     //   borderPane.setCenter(showBookings());
         borderPane.setTop(tabPane);
 
-        //ADDS BOTH EMPLOYEE AND LOGIN INFORMATION
+        //ADD BOTH EMPLOYEE AND LOGIN INFORMATION TO DATABASE
         add.setOnAction(event -> {
             Stage stage1 = new Stage();
             GridPane gridPane = new GridPane();
@@ -224,39 +217,44 @@ public class ManageEmployeeView {
             TextField passwordTextField = new TextField();
 
             //POSITION TEXT AND LABEL ELEMENTS
-            GridPane.setConstraints(name,0,0);
-            GridPane.setConstraints(nameTextField,1,0);
-            GridPane.setConstraints(surname,0,1);
-            GridPane.setConstraints(surnameText,1,1);
-            GridPane.setConstraints(email,0,2);
-            GridPane.setConstraints(emailText,1,2);
-            GridPane.setConstraints(phoneNumber,0,3);
-            GridPane.setConstraints(phoneNumberText,1,3);
-            GridPane.setConstraints(position,0,4);
-            GridPane.setConstraints(positionTextField,1,4);
-            GridPane.setConstraints(userName,0,5);
-            GridPane.setConstraints(userNameTextField,1,5);
-            GridPane.setConstraints(password,0,6);
-            GridPane.setConstraints(passwordTextField,1,6);
-            GridPane.setConstraints(confirm,1,7);
+            GridPane.setConstraints(name, 0, 0);
+            GridPane.setConstraints(nameTextField, 1, 0);
+            GridPane.setConstraints(surname, 0, 1);
+            GridPane.setConstraints(surnameText, 1, 1);
+            GridPane.setConstraints(email, 0, 2);
+            GridPane.setConstraints(emailText, 1, 2);
+            GridPane.setConstraints(phoneNumber, 0, 3);
+            GridPane.setConstraints(phoneNumberText, 1, 3);
+            GridPane.setConstraints(position, 0, 4);
+            GridPane.setConstraints(positionTextField, 1, 4);
+            GridPane.setConstraints(userName, 0, 5);
+            GridPane.setConstraints(userNameTextField, 1, 5);
+            GridPane.setConstraints(password, 0, 6);
+            GridPane.setConstraints(passwordTextField, 1, 6);
+            GridPane.setConstraints(confirm, 1, 7);
 
 
             confirm.setStyle("-fx-font-size: 20");
 
             gridPane.setAlignment(Pos.CENTER);
 
-            gridPane.getChildren().addAll(name,nameTextField,surname,surnameText,email,emailText,phoneNumber,phoneNumberText,position,userName,userNameTextField,password,passwordTextField,positionTextField,confirm);
+            gridPane.getChildren().addAll(name, nameTextField, surname, surnameText, email, emailText, phoneNumber, phoneNumberText, position, userName, userNameTextField, password, passwordTextField, positionTextField, confirm);
 
             gridPane.setHgap(20);
             gridPane.setVgap(30);
-            Scene scene = new Scene(gridPane,1000,650);
+            Scene scene = new Scene(gridPane, 1000, 650);
 
             stage1.setScene(scene);
             stage1.show();
 
 
-            //UPDATE BOTH TABLE VIEWS AND DELETE FROM DATABASE
+            //SAVES THE INFO TO THE DATABASE AND UPDATES THE TABLE VIEWS
             confirm.setOnAction(event1 -> {
+                if(userNameTextField.getText().length() == 0 || nameTextField.getText().length() == 0 ||
+                        surnameText.getText().length() == 0 || emailText.getText().length() == 0 ||
+                        phoneNumberText.getText().length() == 0 || positionTextField.getText().length() == 0) {
+                    updateAlertMessage("All text fields must contain information in order to add an employee to database");
+                }else{
                     manageEmployeeController.addEmployeeAction(userNameTextField.getText(), nameTextField.getText(),
                             surnameText.getText(), emailText.getText(), Integer.parseInt(phoneNumberText.getText()),
                             positionTextField.getText());
@@ -265,12 +263,12 @@ public class ManageEmployeeView {
                     tableView1.setItems(manageEmployeeController.returnLogInInfo());
 
                     stage1.close();
-
+                }
 
             });
-
         });
 
+        //LOG OUT BUTTON
         logOut.setOnAction(event -> {
             primaryStage.close();
             LoginView loginView = new LoginView();
@@ -278,11 +276,18 @@ public class ManageEmployeeView {
 
         });
 
+        goBack.setOnAction(event -> {
+            primaryStage.close();
+            MenuView menuView = new MenuView();
+            menuView.start();
+        });
+
 
         hBox.setAlignment(Pos.CENTER);
-        borderPane.setAlignment(logOut,Pos.TOP_RIGHT);
+        borderPane.setAlignment(logOut, Pos.TOP_RIGHT);
+        borderPane.setAlignment(goBack, Pos.TOP_LEFT);
 
-        Scene scene = new Scene(borderPane,1000,650);
+        Scene scene = new Scene(borderPane, 1000, 650);
 
         primaryStage.setScene(scene);
 
@@ -296,15 +301,15 @@ public class ManageEmployeeView {
         alert.showAndWait();
     }
 
+    //TABLE VIEW FOR EMPLOYEE INFO
     public TableView<EmployeeModel> showBookings() {
-
 
 
         manageEmployeeController.returnEmployeeInfo();
         tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
 
-        javafx.scene.control.TableColumn < EmployeeModel, String > userName = new javafx.scene.control.TableColumn<>("User name");
+        javafx.scene.control.TableColumn<EmployeeModel, String> userName = new javafx.scene.control.TableColumn<>("User name");
         userName.setCellValueFactory(new PropertyValueFactory<>("userName"));
         userName.setMinWidth(100);
 
@@ -330,20 +335,21 @@ public class ManageEmployeeView {
         jobTitle.setCellValueFactory(new PropertyValueFactory<>("jobTitle"));
         jobTitle.setMinWidth(150);
 
-        tableView.getColumns().addAll(userName,name,surname,email,phoneNumber,jobTitle);
+        tableView.getColumns().addAll(userName, name, surname, email, phoneNumber, jobTitle);
         tableView.setItems(manageEmployeeController.returnEmployeeInfo());
 
         return tableView;
 
     }
 
+    //TABLE VIEW FOR LOG IN INFO
     public TableView<LoginViewModel> showLogInInfo() {
 
         ManageEmployeeModel manageEmployeeModel = new ManageEmployeeModel();
         tableView1.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
 
-        javafx.scene.control.TableColumn < LoginViewModel, String > userName = new javafx.scene.control.TableColumn<>("User name");
+        javafx.scene.control.TableColumn<LoginViewModel, String> userName = new javafx.scene.control.TableColumn<>("User name");
         userName.setCellValueFactory(new PropertyValueFactory<>("userName"));
         userName.setMinWidth(50);
 
@@ -351,7 +357,7 @@ public class ManageEmployeeView {
         password.setCellValueFactory(new PropertyValueFactory<>("password"));
         password.setMinWidth(50);
 
-        tableView1.getColumns().addAll(userName,password);
+        tableView1.getColumns().addAll(userName, password);
 
         tableView1.setItems(manageEmployeeModel.getLoginInformation());
 
