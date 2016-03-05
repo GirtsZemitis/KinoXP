161388c909c5ad085c006e8a7166821f9aa548d5
mazurmazurmazur <grzaneczka@gmail.com>
2016-03-05 18:35:26 +0100

    some funccionalities for candysoda sale

--- src/com/KinoXP/controller/BuyFoodViewController.java
+++ src/com/KinoXP/controller/BuyFoodViewController.java
@@ -1,7 +1,11 @@
 package com.KinoXP.controller;
 
+import com.KinoXP.view.BuyFoodView;
+
 /**
  * Created by MazurJestBoski on 2016-03-05.
  */
 public class BuyFoodViewController {
+
+    BuyFoodView buyFoodView = new BuyFoodView();
 }
